4a9fd9cdcc287e66d09c7d36b3a17df4496723ea
Alex Holanda <alex.holanda@outlook.com>
2019-09-17 16:19:29 -0300

    Trocando o comportamento da aplicação usando a anotação @Profile

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmailMock.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmailMock.java
@@ -5,15 +5,15 @@ import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 
-@Profile("prod")
+@Profile("dev")
 @TipoDoNotificador(NivelUrgencia.URGENTE)
 @Component
-public class NotificadorEmail implements Notificador {
+public class NotificadorEmailMock implements Notificador {
 
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
 		
-		System.out.printf("Notificando %s através do e-mail %s: %s\n",
+		System.out.printf("MOCK: Notificando %s através do e-mail %s: %s\n",
 				cliente.getNome(), cliente.getEmail(), mensagem);
 	}
 }
