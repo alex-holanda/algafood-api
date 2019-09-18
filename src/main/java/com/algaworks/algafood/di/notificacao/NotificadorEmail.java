22eeade30e212d6fe106d152eba317267cebab03
Alex Holanda <alex.holanda@outlook.com>
2019-09-18 10:35:00 -0300

    Adicionado spring profiles

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.di.notificacao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
@@ -8,8 +9,14 @@ import com.algaworks.algafood.di.modelo.Cliente;
 @Component
 public class NotificadorEmail implements Notificador {
 
+	@Autowired
+	private NotificadorProperties properties;
+	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
+		
+		System.out.println("PORTA: " + properties.getPortaServidor());
+		System.out.println("HOST: " + properties.getHostServidor());
 		
 		System.out.printf("Notificando %s através do e-mail %s: %s\n",
 				cliente.getNome(), cliente.getEmail(), mensagem);
