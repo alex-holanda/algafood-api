3ba2b76c7619cfb6a3d38503d9141d79dc8455a7
Alex Holanda <alex.holanda@outlook.com>
2019-10-31 14:47:02 -0300

    Adicionado consulta dinamica com jpql

--- src/main/java/com/algaworks/algafood/api/resource/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/resource/TesteController.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.api.resource;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,19 +10,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 @RestController
 @RequestMapping("/teste")
 public class TesteController {
 
-//	@Autowired
-//	private CozinhaRepository cozinhaRepository;
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
 	
 	@GetMapping("/cozinhas/por-nome")
 	public ResponseEntity<List<Cozinha>> cozinhasPorNome(@RequestParam String nome) {
 		
-		List<Cozinha> cozinhas = null;
-//				cozinhaRepository.consultarPorNome(nome);
+		List<Cozinha> cozinhas = cozinhaRepository.findAllByNomeContaining(nome);
 		
 		return ResponseEntity.ok(cozinhas);
 	}
--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -1,9 +1,14 @@
 package com.algaworks.algafood.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long>, RestauranteRepositoryQueries {
 	
+//	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+	List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
 }
